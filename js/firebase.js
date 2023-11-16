b1ea297f9674e16ffddd09506b2a39939420b475
Priyansh Singhal <ps704@snu.edu.in>
2023-11-17 03:00:59 +0530

    added connection.js

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBTEqO02yUaPVaxDyt9YTWJ-ugrYDOJF48",
-  authDomain: "grabbo-974fc.firebaseapp.com",
-  projectId: "grabbo-974fc",
-  storageBucket: "grabbo-974fc.appspot.com",
-  messagingSenderId: "851729407382",
-  appId: "1:851729407382:web:decb90d279f38b6b5f11b7",
-  measurementId: "G-8D49RHVYS0",
-};
